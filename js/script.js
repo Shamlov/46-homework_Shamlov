015922a58a63887554d1ccb848b5d041b056a8c9
Shamlov <pshamlov@gmail.com>
2024-07-01 21:57:28 +0300

    result

--- js/script.js
+++ js/script.js
@@ -1,22 +1,32 @@
 let apiRecipe = undefined
 const btnShowRandom = document.querySelector('#btnShowRandom')
 btnShowRandom.addEventListener('click' , randomRequest )
+const btnShow = document.querySelector('#btnShow')
+btnShow.addEventListener('click', userNumRequest)
+
 let promRecipe
 function randomRequest() {
-    let num = Math.floor(Math.random() * (Math.floor(10) - Math.ceil(1 + 1) + Math.ceil(1)))
+    let num = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
     console.log(num)
     apiRecipe = `https://jellybellywikiapi.onrender.com/api/Recipes/${num}`
     promRecipe = fetch(apiRecipe, {method: 'GET'})
     dataRequest()
 }
 
+function userNumRequest() {
+    let number = +document.querySelector('#number').value
+    apiRecipe = `https://jellybellywikiapi.onrender.com/api/Recipes/${number}`
+    promRecipe = fetch(apiRecipe, {method: 'GET'})
+    dataRequest()
+}
+
 function dataRequest() {
     promRecipe.then((status) => {
-        if(status.ok) {
-            alert('Запрос успешный')
-        } else {
-            alert('Ошибка запрса на сервер')
-        }
+        // if(status.ok) {
+        //     alert('Запрос успешный')
+        // } else {
+        //     alert('Ошибка запрса на сервер')
+        // }
         return status.json() 
     }).then((data) => {
         showDataOnPage(data.description , data.imageUrl, data.directions)
@@ -25,14 +35,11 @@ function dataRequest() {
     
 }
 
-
-
-
 const posts = document.querySelector('#posts')
 function showDataOnPage(description, img, directions) {
     const div = document.createElement('div')
     div.className = 'recipe-block' 
-    posts.append(div)
+    posts.prepend(div)
     console.log(directions.join(' '))
 // решил не заморачиваться и использовал  innerHTML
     div.innerHTML = `        
